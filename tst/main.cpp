e384c11227e6ea4f064127b9d8f6680dccaaef76
FermionicChaos <z3rocaffein3@fastmail.com>
2026-05-09 18:33:39 -0500

    added unit test

---
+++ tst/main.cpp
@@ -0,0 +1,11 @@
+// Unit test entry point for geodesy-logic
+
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    std::cout << "geodesy-logic unit tests starting..." << std::endl;
+
+    // Unit tests go here
+
+    return 0;
+}
